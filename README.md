a961033aa4f39df0435bdf142549f078528c999b
carlos <carloscarmonaalcantara@gmail.com>
2017-09-05 10:19:14 +0200

    build: 0.0.1

---
+++ README.md
@@ -0,0 +1,31 @@
+# File Watcher Trigger for [Runnerty]:
+
+It is possible to set up file system path trigger in Runnerty with the help of auto-magically configured filewatchers. Them are defined with the **condition** property and can be fired through the following actions:
+
+- *add*: when a file is added.
+- *change*: when a file is changed.
+- *unlink*: when a file is deleted.
+- *error*: when there is an error in the file treatment.
+
+### Configuration sample:
+```json
+{
+  "id": "filewatcher_default",
+  "type": "@runnerty-trigger-file-watcher"
+}
+```
+
+### Plan sample:
+```json
+{
+  "id": "filewatcher_default",
+  "file_name": "/path/myfile.txt",
+  "condition": "change"
+}
+```
+
+### Chain input
+This trigger sends to the input of the chain the name of the file watched (file_name).
+
+
+[Runnerty]: http://www.runnerty.io
---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const chokidar = require("chokidar");
+var Trigger = global.TriggerClass;
+
+class triggerFileWatcher extends Trigger {
+  constructor(chain, params) {
+    super(chain, params);
+  }
+
+  start() {
+    let _this = this;
+
+    // Create file watcher:
+    _this.fileWatcher = chokidar.watch(_this.params.file_name, {
+      ignored: /[\/\\](\.|\~)/,
+      persistent: true,
+      usePolling: true,
+      ignoreInitial: true,
+      awaitWriteFinish: {
+        stabilityThreshold: 2000,
+        pollInterval: 150
+      }
+    });
+
+    //Create watch condition:
+    _this.fileWatcher.on(_this.params.condition, (pathfile) => {
+      const checkCalendar = true;
+      const inputValues = {"file_name": pathfile};
+      const customValues = {};
+
+      //Start Chain: Send file_name into inputValues.
+      _this.startChain(checkCalendar, inputValues, customValues)
+      .then(() => {})
+      .catch(err => {
+        _this.logger.error("startChain error (triggerFileWatcher):", err);
+      });
+    });
+  }
+}
+
+module.exports = triggerFileWatcher;
